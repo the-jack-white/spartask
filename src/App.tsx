38a252be6d8b7ce726e60a7d4abb027b90c1f090
the-jack-white <46023638+the-jack-white@users.noreply.github.com>
2023-11-21 02:14:24 +0000

    v0.0.2 - Add link to Github project files

--- src/App.tsx
+++ src/App.tsx
@@ -28,25 +28,34 @@ function App() {
 
   return cookies.isAuthenticated && currentUser ? (
     <div className="main-container">
-      <Header />
-      <AppContainer>
-        <Heading title="Today" date={new Date(Date.now()).toDateString()} />
-        {allTasks &&
-          allTasks
-            .filter((filterT) => filterT.userId === currentUser.id)
-            .map((task) => (
-              <Task
-                key={task.id}
-                id={task.id}
-                userId={currentUser.id}
-                timestamp={task.timestamp}
-                title={task.title}
-                description={task.description}
-                completed={task.completed}
-              />
-            ))}
-        <AddTask />
-      </AppContainer>
+      <div className="main-sub-container">
+        <Header />
+        <AppContainer>
+          <Heading title="Today" date={new Date(Date.now()).toDateString()} />
+          {allTasks &&
+            allTasks
+              .filter((filterT) => filterT.userId === currentUser.id)
+              .map((task) => (
+                <Task
+                  key={task.id}
+                  id={task.id}
+                  userId={currentUser.id}
+                  timestamp={task.timestamp}
+                  title={task.title}
+                  description={task.description}
+                  completed={task.completed}
+                />
+              ))}
+          <AddTask />
+        </AppContainer>
+      </div>
+      <a
+        className="app-project-github"
+        href="https://github.com/the-jack-white/spartask"
+        target="_blank"
+      >
+        Redirect to Github project
+      </a>
     </div>
   ) : (
     <AuthContainer>
